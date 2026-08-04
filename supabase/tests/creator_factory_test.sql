2b5b1d2fd04aa0855a9ce6b59c823b8b78891809
Alisia777 <artiukhins@gmail.com>
2026-08-04 16:18:10 +0300

    test: count market learning RPC

--- supabase/tests/creator_factory_test.sql
+++ supabase/tests/creator_factory_test.sql
@@ -154,7 +154,7 @@ select is(
       and procedure.proname like 'creator_%'
       and has_function_privilege('authenticated', procedure.oid, 'execute')
   ),
-  90,
+  91,
   'authenticated can execute all creator RPCs'
 );
 
